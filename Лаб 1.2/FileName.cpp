1a7c4e7bc90ef8ecb504ae1b0602c6cb914dd22b
Степан Семыкин <semykinstepan2@gmail.com>
2023-04-04 23:36:19 +0400

    Добавьте файлы проекта.

---
+++ Лаб 1.2/FileName.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+
+using namespace std;
+
+struct TreeNode
+{
+	double data;
+	TreeNode* right, * left;
+};
+
+class MySet
+{
+	private:
+		TreeNode* root;
+
+		void _clear(TreeNode* root)
+		{
+			if (root)
+			{
+				_clear(root->left);
+				_clear(root->right);
+				delete root;
+			}
+			root = nullptr;
+			root->left = nullptr;
+			root->right = nullptr;
+		}
+
+		TreeNode* _copy(TreeNode* other)
+		{
+			if (!other) return nullptr;
+			TreeNode* tmp = new TreeNode;
+			tmp->data = other->data;
+			tmp->left = _copy(other->left);
+			tmp->right = _copy(other->right);
+			return tmp;
+		}
+
+	public:
+		MySet()
+		{
+			root = nullptr;
+			root->left = nullptr;
+			root->right = nullptr;
+		}
+
+		MySet(const MySet& m)
+		{
+			root = _copy(m.root);
+		}
+
+		~MySet()
+		{
+			_clear(root);
+		}
+
+		void clear()
+		{
+			_clear(root);
+		}
+
+		MySet& operator = (const MySet& m)
+		{
+			if (!root) root = _copy(m.root);
+			else 
+			{
+				_clear(root);
+				root = _copy(m.root);
+			}
+			return *this;
+		}
+
+
+};
